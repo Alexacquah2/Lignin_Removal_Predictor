8eb747b6d3773a65c6c73cd880580cfc12c620de
Alexacquah2 <aacqua2@uic.edu>
2025-06-18 13:11:18 -0500

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,8 +7,11 @@ df = pd.read_csv('../data/biomass_lignin_removal.csv')
 # Rename the target column for consistency and code safety
 df = df.rename(columns={"Delignification (%)": "lignin_removal"})
 
-# (Optional) Show the first 5 rows for inspection
-print(df.head())
+# Set pandas to display all rows
+pd.set_option('display.max_rows', None)
+
+# Show all rows for inspection
+print(df)
 
 # Save the cleaned data
 df.to_csv('../data/cleaned_biomass_lignin_removal.csv', index=False)
